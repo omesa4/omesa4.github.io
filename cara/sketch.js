ec66837353752cf8931259d68e0320cc01d435d5
omesa4 <omesa4@iespompeufabra.cat>
2024-09-18 10:19:17 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
+ createCanvas(600,400);
  // put setup code here
 }
 
 function draw() {
+ background(127);
+   fill(0,255,0);
+ ellipse(250,200,80,20);
+ ellipse(350,200,80,20);
+ fill(255,0,0);
+ ellipse(300,200,220,300);
   // put drawing code here
 }
